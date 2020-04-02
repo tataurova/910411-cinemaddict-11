ac53d93020fc373fa4a9db4106901e34d982d083
tataurova <tataurovaoe@yandex.ru>
2020-04-02 18:29:22 +0300

    Правка для eslint

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ const createFilmBlockTemplate = () => {
 
 const createFilmListTemplate = ({title, isExtra = false}) => {
   return (
-    `<section class="films-list${isExtra ? "--extra" : ""}">
+    `<section class="films-list${isExtra ? `--extra` : ``}">
       <h2 class="films-list__title">${title}</h2>
       <div class="films-list__container"></div>
      </section>`
